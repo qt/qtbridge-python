474097cdadfd183314ba806ce21d40cd9b91eff4
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2026-02-02 10:45:39 +0100

    Examples: Modify counter example
    - The counter example is modified to demonstrate signal support in
      QtBridge. Instead of using the default property change notifications,
      created with property bindings, the example now defines explicit
      signals. Since the explicitly defined signal is also a Changed
      signal, it is automatically registered as the notify signal for the
      property.
    
    Task-number: QTBRIDGES-24
    Change-Id: I82361b2277a92e3955fcaad8fa4d36eefd272574
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>

--- examples/counter/counter.py
+++ examples/counter/counter.py
@@ -2,10 +2,11 @@
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 import sys
-
-from QtBridge import qtbridge, bridge_type
+from QtBridge import qtbridge, bridge_type, Signal
 
 class CounterModel:
+    countChanged = Signal(int)
+
     def __init__(self):
         self._count = 0
 
@@ -14,9 +15,12 @@ class CounterModel:
         return self._count
 
     @count.setter
-    def count(self, value):
-        print(f"CounterModel count changed from {self._count} to {value}")
-        self._count = int(value)
+    def count(self, value: int):
+        if self._count != value:
+            print(f"CounterModel count changed from {self._count} to {value}")
+            self._count = value
+            # Manually emit the signal instead of using the auto property change notification
+            self.countChanged.emit(self._count)
 
 @qtbridge(module="CounterModel")
 def main():
